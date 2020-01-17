df725295af10b4c8468160cf4157dc89f7d048db
Diogo Cezar Teixeira Batista <diogo.batista@meifacil.com>
2020-01-17 20:23:59 -0300

    Add mobile stuffs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -48,6 +48,7 @@ export default function Main({ navigation }) {
     });
     setDevs(response.data.devs);
   };
+  const handleRegionChanged = () => {};
   if (!currentRegion) return null;
   return (
     <>
